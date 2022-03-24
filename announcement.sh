c296116777cadef7db29e78d03fdb20362c5eb62
c3lphie <c3lphie@protonmail.com>
2022-03-24 20:02:28 +0100

    Update announcement.sh

--- announcement.sh
+++ announcement.sh
@@ -1,6 +1,9 @@
+
+echo "================================="
 echo $2
 echo $3
+echo "================================="
 
 
-curl -X POST $1 -d "content=:pepefrog: @everyone :pepefrog:
+curl -X POST $1 -d "content=:smile: @everyone :smile:
 New post! Check it out here: [$2]($3)"
